82ef4d384120d441685b7935bb53017f28fe5db2
Mikhail Potapov <86321783+mishapotap@users.noreply.github.com>
2021-12-17 15:09:15 +0300

    charinfo_updated

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -14,7 +14,7 @@ class MarvelService {
 
     getAllCharacters = async () => {
         const res = await this.getData(
-            `${this._apiBase}characters?limit=9&offset=140&${this._apiKey}`
+            `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}` // 140 210
         );
         return res.data.results.map(this._transformCharacter); // Return massive of characters
     };
@@ -30,15 +30,14 @@ class MarvelService {
         return {
             id: char.id,
             name:
-                char.name.length > 20
-                    ? `${char.name.slice(0, 21)}...`
-                    : char.name,
+                char.name.length > 20 ? `${char.name.slice(0, 21)}...` : char.name,
             description: char.description
                 ? `${char.description.slice(0, 210)}...`
                 : "Unfortunately this character has no description",
             thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url,
+            comics: char.comics.items.slice(0, 10), // massive
         };
     }; // Transform character
 }
