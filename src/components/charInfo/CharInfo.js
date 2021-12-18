176ed1e576d7fdae6e66867ca75433d5048e4a58
Mikhail Potapov <86321783+mishapotap@users.noreply.github.com>
2021-12-18 19:24:47 +0300

    propTypes_added

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import PropTypes from "prop-types";
 import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
@@ -118,6 +119,9 @@ const View = ({ char }) => {
             </ul>
         </>
     );
+};
+CharInfo.propTypes = {
+    charId: PropTypes.number,
 };
 
 export default CharInfo;
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import PropTypes from "prop-types";
 import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
@@ -108,5 +109,8 @@ class CharList extends Component {
         );
     }
 }
+CharList.propTypes = {
+    onSelectedChar: PropTypes.func,
+};
 
 export default CharList;
