b985ef7063f1d1970485abc98f30073d7d776b83
Mikhail Potapov <86321783+mishapotap@users.noreply.github.com>
2022-01-23 20:43:40 +0300

    MarvelService updated for Comics

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -19,6 +19,16 @@ const useMarvelService = () => {
         return _transformCharacter(res.data.results[0]);
     }; // Возвращает одного персонажа
 
+    const getAllComics = async (offset = 0) => {
+        const res = await request(`${_apiBase}comics?limit=8&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_transformComics);
+    }; // Вовзращает массив комиксов
+
+    const getComics = async (id) => {
+        const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+        return _transformComics(res.data.results[0]);
+    }; // Вовзращает массив комиксов
+
     const _transformCharacter = (char) => {
         return {
             id: char.id,
@@ -29,11 +39,25 @@ const useMarvelService = () => {
             thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url,
-            comics: char.comics.items.slice(0, 10), // massive
+            comics: char.comics.items.slice(0, 10), // Массив
         };
     }; // Трансформирует персоанажа в нужный нам формат
 
-    return { loading, error, getAllCharacters, getCharacter, clearError };
+    const _transformComics = (comics) => {
+        return {
+            id: comics.id,
+            title: comics.title,
+            description: comics.description || "Unfortunately this comics has no description",
+            pageCount: comics.pageCount
+                ? `${comics.pageCount} pages`
+                : "No information about the number of pages",
+            thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
+            language: comics.textObjects.language || "en-us",
+            price: comics.prices.price,
+        };
+    }; // Трансформирует комиксы в нужный нам формат
+
+    return { loading, error, getAllCharacters, getCharacter, clearError, getAllComics };
 };
 
 export default useMarvelService;
