a285f39d9b61fe01e42b41d20de55c23dc633494
Mikhail Potapov <86321783+mishapotap@users.noreply.github.com>
2022-01-23 23:33:31 +0300

    Routing added

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,43 +1,24 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-
-import decoration from "../../resources/img/vision.png";
-import AppBanner from "../appBanner/AppBanner";
-import ComicsList from "../comicsList/ComicsList";
+import { MainPage, ComicsPage } from "../pages";
 import SingleComic from "../singleComic/SingleComic";
 
 const App = () => {
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }; // Установили id выбранного персонажа в state
-
     return (
-        <div className="app">
-            <AppHeader />
-            <main>
-                {/* <ErrorBoundary>
-                    <RandomChar />
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected} />
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar} />
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision" /> */}
-                <AppBanner />
-                <ComicsList />
-                {/* <SingleComic /> */}
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader />
+                <main>
+                    <Routes>
+                        <Route path="/" element={<MainPage />} />
+                        <Route path="comics" element={<ComicsPage />} />
+                        {/* <Route path="/comics/comicsid">
+                            <SingleComic />
+                        </Route> */}
+                    </Routes>
+                </main>
+            </div>
+        </Router>
     );
 };
 
