be7fc7bc2d1705427b91f38af560ce0d282836ba
Mikhail Potapov <86321783+mishapotap@users.noreply.github.com>
2022-01-24 00:39:57 +0300

    404 page Added

---
+++ src/components/pages/404.js
@@ -0,0 +1,29 @@
+import img from "../../resources/img/404.png";
+import { Link } from "react-router-dom";
+
+const Page404 = () => {
+    return (
+        <div>
+            <img src={img} alt="404_page_not found" />
+            <Link
+                style={{
+                    display: "block",
+                    textAlign: "center",
+                    fontWeight: "bold",
+                    fontSize: "24px",
+                    margin: "20px auto 0",
+                    width: "250px",
+                    color: "#ffffff",
+                    backgroundColor: "#9F0013",
+                    textTransform: "uppercase",
+                    cursor: "cointer",
+                }}
+                to="/"
+            >
+                Back to main page
+            </Link>
+        </div>
+    );
+};
+
+export default Page404;
